2925cd81a5bd0c3ae00f2b5a07f9241677a96c8a
justi <justi@DESKTOP-O7K8SPS>
2018-08-10 08:46:02 -0400

    k

---
+++ CardParser/src/CardParser.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CardParser.cpp
+// Author      : Nathaniel-Joel Parizi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello" << endl; // prints Hello
+	return 0;
+}
